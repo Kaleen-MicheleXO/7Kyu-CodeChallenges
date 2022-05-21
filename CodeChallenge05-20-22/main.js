313260b204433e1a82bc4fd028722717902c745b
Kaleen-MicheleXO <Kaleen.Moon1@gmail.com>
2022-05-20 23:29:35 -0400

    makeBackronym solution 05-20-22

---
+++ CodeChallenge05-20-22/main.js
@@ -0,0 +1,18 @@
+// Complete the function to create backronyms. Transform the given string (without spaces) to a backronym, using the preloaded dictionary and return a string of words, separated with a single space (but no trailing spaces)
+// The keys of the preloaded dictionary are uppercase letters A-Z and the values are predetermined words, for example:
+
+// dict["P"] == "perfect"
+
+// Examples
+// "dgm" ==> "disturbing gregarious mustache"
+// "lkj" ==> "literal klingon joke"
+
+var makeBackronym = function (string) {
+  //preload variable: dict
+  //your code here
+  return string
+    .toUpperCase()
+    .split("")
+    .map((w) => dict[w])
+    .join(" ");
+};
