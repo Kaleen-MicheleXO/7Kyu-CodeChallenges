c620885c86a68b59625da350da3b422004c6a8b9
Kaleen-MicheleXO <Kaleen.Moon1@gmail.com>
2022-05-28 12:19:06 -0400

    Fix string case Solution 05-28-22

---
+++ CodeChallenge05-28-22/main.js
@@ -0,0 +1,19 @@
+// In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+// make as few changes as possible.
+// if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+
+// For example:
+// solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+// solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+// solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+function solve(s) {
+  let cap = 0;
+  let low = 0;
+  for (let i = 0; i <= s.length - 1; i++) {
+    if (s[i] === s[i].toUpperCase()) {
+      cap += 1;
+    } else low += 1;
+  }
+  return cap > low ? s.toUpperCase() : s.toLowerCase();
+}
