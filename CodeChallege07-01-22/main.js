9fdf2b3556e8c1a64e7bc90cff63a2f6f905b978
Kaleen-MicheleXO <Kaleen.Moon1@gmail.com>
2022-07-01 15:25:16 -0400

    Sorted? yes? no? how? 07-01-22

--- CodeChallege07-01-22/main.js
+++ CodeChallege07-01-22/main.js
@@ -11,9 +11,12 @@ isSortedAndHow([15, 7, 3, -8]), "yes, descending";
 isSortedAndHow([4, 2, 30]), "no";
 
 function isSortedAndHow(array) {
-  return array.every((x, i) => array[i] >= array[i - 1])
+  return array.every((x, i) => i == 0 || array[i] >= array[i - 1])
     ? "yes, ascending"
-    : array.every((x, i) => array[i] <= array[i - 1])
+    : array.every((x, i) => i == 0 || array[i] <= array[i - 1])
     ? "yes, descending"
     : "no";
 }
+
+//The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value
+//the condition on the left is just an edge case to avoid an error when using arr[i-1]
