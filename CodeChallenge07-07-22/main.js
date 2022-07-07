b0a51ded7ff4d24444aefbf3f1399900ca065f76
Kaleen-MicheleXO <Kaleen.Moon1@gmail.com>
2022-07-07 16:46:21 -0400

    Mumbling Solution 07-07-22

---
+++ CodeChallenge07-07-22/main.js
@@ -0,0 +1,21 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+// accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+// accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+// accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+// accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Ccccccc"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+  let str = s.toLowerCase();
+  let arr = [];
+  for (let i = 0; i <= str.length - 1; i++) {
+    arr.push(str[i].toUpperCase().concat(str[i].repeat(i)));
+  }
+  return arr.join("-");
+}
